970d501cacf549c9a5a0130a6bf0edd8fb7073ed
unknown <eduardoevperez@gmail.com>
2022-01-31 22:29:30 -0700

    Change primary keys to foreign keys because some columns are not unique

--- schema.sql
+++ schema.sql
@@ -17,13 +17,12 @@ CREATE TABLE IF NOT EXISTS employees (
 );
 
 CREATE TABLE IF NOT EXISTS dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
 CREATE TABLE IF NOT EXISTS salaries (
@@ -40,8 +39,7 @@ CREATE TABLE IF NOT EXISTS titles (
     title VARCHAR(40) NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    PRIMARY KEY (emp_no)
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 CREATE TABLE IF NOT EXISTS dept_emp (
@@ -50,6 +48,5 @@ CREATE TABLE IF NOT EXISTS dept_emp (
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no)
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
